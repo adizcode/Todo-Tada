b4039b2752901fce9b5843bfeabbdf4c71c83abd
Advitiay Anand <advitiay.a@gmail.com>
2022-05-20 20:14:00 +0530

    Make Inter the default font

--- app/src/main/java/com/github/adizcode/todo_tada/view/theme/Type.kt
+++ app/src/main/java/com/github/adizcode/todo_tada/view/theme/Type.kt
@@ -1,34 +1,19 @@
 package com.github.adizcode.todo_tada.view.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.github.adizcode.todo_tada.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    h4 = TextStyle(
-        fontFamily = FontFamily.Monospace,
-        fontWeight = FontWeight.Normal,
-        fontSize = 34.sp,
-        letterSpacing = 0.25.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = Inter
 )
