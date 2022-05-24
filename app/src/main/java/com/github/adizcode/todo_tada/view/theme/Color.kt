f61a0a74a141ad7f905f821dc2ccae5c220b85f2
Advitiay Anand <advitiay.a@gmail.com>
2022-05-24 20:12:16 +0530

    Change secondary color

--- app/src/main/java/com/github/adizcode/todo_tada/view/theme/Color.kt
+++ app/src/main/java/com/github/adizcode/todo_tada/view/theme/Color.kt
@@ -2,6 +2,8 @@ package com.github.adizcode.todo_tada.view.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Navy = Color(0xFF3F4EA0)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
--- app/src/main/java/com/github/adizcode/todo_tada/view/theme/Theme.kt
+++ app/src/main/java/com/github/adizcode/todo_tada/view/theme/Theme.kt
@@ -9,13 +9,13 @@ import androidx.compose.runtime.Composable
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Navy
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Navy
 
     /* Other default colors to override
     background = Color.White,
